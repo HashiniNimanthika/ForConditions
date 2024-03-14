818f9b17da2eb94da9a52d0ba68cc41011a14556
hashi <hashi@HASHINI>
2024-03-14 19:50:32 +1300

    Add project files.

---
+++ ForConditions/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace ForConditions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input;
+            Console.WriteLine("Hey user please enter a value that will be used to iterate");
+            input= Convert.ToInt32(Console.ReadLine());
+            int i;
+            for (i = 1; i <= 15; i++)
+            {
+              Console.WriteLine("C# For Loop : Ireration" + i);
+            }
+        }
+    }
+}
+    
